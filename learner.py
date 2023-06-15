f74c19c3cadb9d9cb99ecb830b4ec005aaf65358
Maximilian Schmidt <maximilian@schmidt.so>
2023-06-15 15:28:47 +0200

    Cleanup learner

--- learner.py
+++ learner.py
@@ -1,15 +1,26 @@
+from pathlib import Path
+
 import lightning
-import pandas as pd
+import polars as pl
 import torch
+from torch.utils.data import DataLoader, Dataset, random_split
 
 
-class AccelerometerDataset(torch.utils.data.Dataset):
-    def __init__(self, csv_file):
-        # Load data
-        data = pd.read_csv(csv_file)
-        # 2: y-amplitude, 4: z-amplitude, 6: growth rate
-        self.inputs = data.iloc[:, [2,4,6]].values 
-        self.targets = data.iloc[:, -1].values
+class AccelerometerDataset(Dataset):
+    def __init__(self, root: Path | str, train: bool = True):
+        root = Path(root)
+        if train:
+            filename = "train.csv"
+        else:
+            filename = "test.csv"
+        data = pl.read_csv(root / filename)
+        print(data)
+        self.inputs = data.select(
+            "y-Amplitude",
+            "z-Amplitude",
+            "Growth-rate",
+        ).to_numpy()
+        self.targets = data.select("Estimated-Failure-Time").to_numpy()
 
     def __len__(self):
         return len(self.targets)
@@ -18,7 +29,7 @@ class AccelerometerDataset(torch.utils.data.Dataset):
         inputs = torch.tensor(self.inputs[idx], dtype=torch.float32)
         targets = torch.tensor(self.targets[idx], dtype=torch.float32)
         return inputs, targets
-    
+
 
 class AccelerometerData(lightning.LightningDataModule):
     def __init__(self):
@@ -26,53 +37,67 @@ class AccelerometerData(lightning.LightningDataModule):
 
     def setup(self, stage=None):
         if stage == "fit" or stage == "validate" or stage is None:
-            full_training_dataset = AccelerometerDataset("train.csv")
+            full_training_dataset = AccelerometerDataset(
+                "data/accelerometer/",
+                train=True,
+            )
             train_len = int(0.8 * len(full_training_dataset))
             val_len = len(full_training_dataset) - train_len
-            self.train_dataset, self.val_dataset = torch.utils.data.random_split(full_training_dataset, [train_len, val_len])
+            self.train_dataset, self.val_dataset = random_split(
+                full_training_dataset,
+                [train_len, val_len],
+            )
         if stage == "test" or stage is None:
-            self.test_dataset = AccelerometerDataset("test.csv")
+            self.test_dataset = AccelerometerDataset("data/accelerometer", train=False)
 
     def train_dataloader(self):
-        return torch.utils.data.DataLoader(self.train_dataset, batch_size=32, shuffle=True)
+        return DataLoader(self.train_dataset, batch_size=64, shuffle=True)
 
     def val_dataloader(self):
-        return torch.utils.data.DataLoader(self.val_dataset, batch_size=32)
-    
+        return DataLoader(self.val_dataset, batch_size=64)
+
     def test_dataloader(self):
-        return torch.utils.data.DataLoader(self.test_dataset, batch_size=32)
-    
+        return DataLoader(self.test_dataset, batch_size=64)
+
 
 class MLP(lightning.LightningModule):
-    def __init__(self):
+    def __init__(self, learning_rate: float = 1e-3):
         super().__init__()
+        self.learning_rate = learning_rate
         self.model = torch.nn.Sequential(
             torch.nn.Linear(3, 25),
             torch.nn.Sigmoid(),
             torch.nn.Linear(25, 1),
-            torch.nn.ReLU()
+            torch.nn.ReLU(),
         )
 
     def forward(self, x):
         return self.model(x)
 
-    def training_step(self, batch, batch_idx):
+    def _shared_eval_step(self, batch):
         inputs, targets = batch
-        outputs = self.forward(inputs)
+        outputs = self(inputs)
         loss = torch.nn.functional.mse_loss(outputs, targets)
+        return loss
+
+    def training_step(self, batch, _):
+        loss = self._shared_eval_step(batch)
         self.log("train/loss", loss)
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        inputs, targets = batch
-        outputs = self.forward(inputs)
-        loss = torch.nn.functional.mse_loss(outputs, targets)
+    def validation_step(self, batch, _):
+        loss = self._shared_eval_step(batch)
         self.log("validate/loss", loss)
         return loss
 
+    def test_step(self, batch, _):
+        loss = self._shared_eval_step(batch)
+        self.log("test/loss", loss)
+        return loss
+
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
-    
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
 
 if __name__ == "__main__":
     data = AccelerometerData()
--- split_train_test.py
+++ split_train_test.py
@@ -1,13 +1,72 @@
-import pandas as pd
+#!/usr/bin/env python
 
-def split(csv_file):
-    data = pd.read_csv(csv_file)
+import argparse
+import os
+from pathlib import Path
+from typing import Optional
 
-    # split data into training and test sets
-    train = data.sample(frac=0.8, random_state=0)
-    test = data.drop(train.index)
+import polars as pl
+from sklearn.model_selection import train_test_split
 
-    train.to_csv("train.csv")
-    test.to_csv("test.csv")
 
-split("processed_data.csv")
+def split(
+    csv_file: Path,
+    train_filename,
+    test_filename,
+    test_size,
+    output_dir: Optional[Path] = None,
+    random_state: int = 42,
+):
+    data = pl.read_csv(csv_file)
+
+    train, test = train_test_split(
+        data,
+        test_size=test_size,
+        random_state=random_state,
+    )
+
+    if output_dir is None:
+        output_dir = Path(os.path.dirname(csv_file))
+    train.write_csv(output_dir / train_filename)
+    test.write_csv(output_dir / test_filename)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "csv_file",
+        type=Path,
+        help="csv file to split",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=Path,
+        help="output directory",
+        default=None,
+    )
+    parser.add_argument(
+        "--train_filename",
+        type=str,
+        help="name of the test dataset file",
+        default="train.csv",
+    )
+    parser.add_argument(
+        "--test_filename",
+        type=str,
+        help="name of the training dataset file",
+        default="test.csv",
+    )
+    parser.add_argument(
+        "--test_size",
+        type=float,
+        help="relative size of the test dataset",
+        default=0.2,
+    )
+    arguments = parser.parse_args()
+    split(
+        arguments.csv_file,
+        train_filename=arguments.train_filename,
+        test_filename=arguments.test_filename,
+        test_size=arguments.test_size,
+        output_dir=arguments.output_dir,
+    )
