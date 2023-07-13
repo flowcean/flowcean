ae1867a0124164349aa14b2e4034c2aacdb4b1e0
Stephan Balduin <stephan.balduin@offis.de>
2023-07-13 09:52:44 +0200

    Fixed an error in the dyna_loader

--- src/agenc/dyna_loader.py
+++ src/agenc/dyna_loader.py
@@ -10,12 +10,10 @@ def load_function(function_path: str) -> Callable:
     return getattr(module, function_name)
 
 
-def load_class(
-    class_path: str, *args: Optional[Any], **kwargs: Optional[Any]
-) -> Any:
+def load_class(class_path: str, kwargs: Optional[Any]) -> Any:
     """Dynamically load a class with arguments and keyword arguments."""
     class_module, class_name = class_path.rsplit(".", 1)
     module = import_module(class_module)
     class_reference = getattr(module, class_name)
 
-    return class_reference(*args, **kwargs)
+    return class_reference(**kwargs)
