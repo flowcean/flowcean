79adcd06cf245238c0b6d373ebc2e1f6682b3bc0
Markus Knitt <markus.knitt@tuhh.de>
2023-11-16 10:37:37 +0100

    Changed return value of DummyMetric to zero

--- src/agenc/metrics/dummy_metric.py
+++ src/agenc/metrics/dummy_metric.py
@@ -9,5 +9,5 @@ rng = np.random.default_rng(0)
 
 
 class DummyMetric(Metric):
-    def __call__(self, y_true: NDArray[Any], y_predicted: NDArray[Any]) -> Any:
-        return np.sum(y_true - y_predicted) * rng.random()
+    def __call__(self, _y_true: NDArray[Any], _y_predicted: NDArray[Any]) -> Any:
+        return 0
