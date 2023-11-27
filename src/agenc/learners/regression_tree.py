9b39dadbed354411103535c1ac4e4dadfd93d6ec
Maximilian Schmidt <maximilian@schmidt.so>
2023-11-27 11:18:41 +0100

    export dot graphs in regression trees

--- src/agenc/learners/regression_tree.py
+++ src/agenc/learners/regression_tree.py
@@ -1,12 +1,15 @@
+import logging
 from pathlib import Path
 from typing import Any, cast
 
 import joblib
 import polars as pl
 from numpy.typing import NDArray
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor, export_graphviz
 
 from agenc.core import Learner
+
+logger = logging.getLogger(__name__)
 
 
 class RegressionTree(Learner):
@@ -17,8 +20,14 @@ class RegressionTree(Learner):
 
     regressor: DecisionTreeRegressor
 
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
+    def __init__(
+        self,
+        *args: Any,
+        dot_graph_export_path: None | str = None,
+        **kwargs: Any,
+    ) -> None:
         self.regressor = DecisionTreeRegressor(*args, **kwargs)
+        self.dot_graph_export_path = dot_graph_export_path
 
     def train(
         self,
@@ -29,6 +38,15 @@ class RegressionTree(Learner):
         input_data = data.select(inputs).to_numpy()
         output_data = data.select(outputs).to_numpy()
         self.regressor.fit(input_data, output_data)
+        if self.dot_graph_export_path is not None:
+            logger.info(
+                "Exporting decision tree graph to"
+                f" {self.dot_graph_export_path}"
+            )
+            export_graphviz(
+                self.regressor,
+                out_file=self.dot_graph_export_path,
+            )
 
     def predict(self, inputs: NDArray[Any]) -> NDArray[Any]:
         return cast(NDArray[Any], self.regressor.predict(inputs))
