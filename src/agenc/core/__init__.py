74f16c481033f2314895f133125231748446680c
Maximilian Schmidt <maximilian@schmidt.so>
2023-12-21 20:00:04 +0100

    remove dataset import

--- src/agenc/core/__init__.py
+++ src/agenc/core/__init__.py
@@ -1,13 +1,11 @@
 __all__ = [
     "DataLoader",
-    "Dataset",
     "Learner",
     "Metric",
     "Transform",
 ]
 
 from .data_loader import DataLoader
-from .dataset import Dataset
 from .learner import Learner
 from .metric import Metric
 from .transform import Transform
