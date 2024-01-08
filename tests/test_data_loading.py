c0680188664e798e36768486894902502f2b343f
Markus Knitt <markus.knitt@tuhh.de>
2024-01-08 11:27:50 +0100

    Simplified assert condition

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -15,7 +15,7 @@ class TestDataloading(unittest.TestCase):
             datapath = Path(f.name)
             dataloader = CsvDataLoader(path=datapath)
             loaded_data = dataloader.load()
-            assert loaded_data.frame_equal(data) is True
+            assert loaded_data.frame_equal(data)
 
 
 if __name__ == "__main__":
