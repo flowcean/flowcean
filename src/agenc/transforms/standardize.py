2ca82df9920aed3949256f894639b463f9a418b3
Maximilian Schmidt <maximilian@schmidt.so>
2024-01-29 11:43:04 +0100

    standardize can only transform floats

--- src/agenc/transforms/standardize.py
+++ src/agenc/transforms/standardize.py
@@ -1,6 +1,5 @@
-import typing
-
 import polars as pl
+from polars.type_aliases import PythonLiteral
 
 from agenc.core import Transform
 
@@ -22,16 +21,16 @@ class Standardize(Transform):
         std: The standard deviation :math:`\sigma` of each feature.
     """
 
-    mean: None | dict[str, typing.Any]
-    std: None | dict[str, typing.Any]
+    mean: None | dict[str, float]
+    std: None | dict[str, float]
 
     def __init__(self) -> None:
         self.mean = None
         self.std = None
 
     def fit(self, data: pl.DataFrame) -> None:
-        self.mean = {c: data[c].mean() for c in data.columns}
-        self.std = {c: data[c].std() for c in data.columns}
+        self.mean = {c: __as_float(data[c].mean()) for c in data.columns}
+        self.std = {c: __as_float(data[c].std()) for c in data.columns}
 
     def __call__(self, data: pl.DataFrame) -> pl.DataFrame:
         if self.mean is None or self.std is None:
@@ -44,3 +43,13 @@ class Standardize(Transform):
                 for c in data.columns
             ],
         )
+
+
+def __as_float(value: PythonLiteral | None) -> float:
+    if value is None:
+        raise ValueError("Value cannot be None.")
+    if isinstance(value, float):
+        return value
+    if isinstance(value, int):
+        return float(value)
+    raise ValueError("Value must be a float.")
