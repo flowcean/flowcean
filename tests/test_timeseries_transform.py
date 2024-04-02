16b23458536c25b85a8179cffba99e43c2051d94
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-04-02 09:04:22 +0200

    Added test case for single column flatten

--- tests/test_timeseries_transform.py
+++ tests/test_timeseries_transform.py
@@ -37,6 +37,37 @@ class TimeSeriesTransform(unittest.TestCase):
             check_column_order=False,
         )
 
+    def test_flatten_selected(self) -> None:
+        flatten_transform = Flatten(features=["a"])
+
+        data_frame = pl.DataFrame(
+            [
+                {
+                    "a": [0, 1],
+                    "b": [0, 1, 2],
+                    "c": 3,
+                },
+                {
+                    "a": [1, 2],
+                    "b": [3, 4, 5],
+                    "c": 6,
+                },
+            ],
+        )
+
+        transformed_data = flatten_transform.transform(data_frame)
+
+        assert_frame_equal(
+            transformed_data,
+            pl.DataFrame(
+                [
+                    {"a_0": 0, "a_1": 1, "b": [0, 1, 2], "c": 3},
+                    {"a_0": 1, "a_1": 2, "b": [3, 4, 5], "c": 6},
+                ],
+            ),
+            check_column_order=False,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
