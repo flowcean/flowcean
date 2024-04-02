dc6309fde091abc552c93d3154ced13019946f44
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-04-02 08:59:26 +0200

    Updated docstring with example dataset

--- src/flowcean/transforms/flatten.py
+++ src/flowcean/transforms/flatten.py
@@ -9,11 +9,25 @@ logger = logging.getLogger(__name__)
 
 
 class Flatten(Transform):
-    """Flatten all timeseries in a DataFrame to individual features.
+    """Flatten all time series in a DataFrame to individual features.
 
-    The given DataFrame's timeseries are converted into individual features,
+    The given DataFrame's time series are converted into individual features,
     with each time step creating a new feature. This transform will change the
     order of the columns in the resulting dataset.
+
+    For example the dataset
+
+    series_data | A  | B
+    ------------|----|----
+    [0, 1, 2]   | 42 | 43
+    [3, 4, 5]   | 44 | 45
+
+    gets flattend into the dataset
+
+    series_data_0 | series_data_1 | series_data_2 | A  | B
+    --------------|---------------|---------------|----|----
+    0             | 1             | 2             | 42 | 43
+    3             | 4             | 5             | 42 | 43
     """
 
     def __init__(self) -> None:
@@ -26,7 +40,7 @@ class Flatten(Transform):
                 continue  # Skip any non timeseries features
             flatten_df = self.__flatten_feature__(data[feature_name])
             data = data.drop(feature_name).hstack(flatten_df)
-        return data
+        return data.rechunk()
 
     def __flatten_feature__(self, feature_series: pl.Series) -> pl.DataFrame:
         # First check if all entries have the same shape
