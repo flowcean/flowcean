b9cfbcd0dba6be4265483f2f41665925b0b07332
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-07-31 15:01:19 +0200

    Add `to_time_series` method and test

--- src/flowcean/core/environment/offline.py
+++ src/flowcean/core/environment/offline.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from typing import TYPE_CHECKING
+
+import polars as pl
 
 from .base import Environment
 from .streaming import StreamingOfflineData
-
-if TYPE_CHECKING:
-    import polars as pl
 
 
 class OfflineEnvironment(Environment):
@@ -49,3 +47,53 @@ class OfflineEnvironment(Environment):
         from .stack import StackEnvironment
 
         return StackEnvironment(self, other)
+
+    def to_time_series(
+        self, time_feature: str | dict[str, str]
+    ) -> OfflineEnvironment:
+        """Convert this envrionment to a time series.
+
+        Args:
+            time_feature: The feature in this environment that represents the
+                time vector. Either a string if all series share a common time
+                vector, or a dictionary where the keys are the value features
+                and the values are the corresponding time vector feature names.
+
+        Returns:
+            A OfflineEnvrionment with exactly one sample containg the source
+            environment as a time series.
+        """
+        from flowcean.environments.dataset import Dataset
+
+        # Get the underlaying dataframe
+        data = self.get_data()
+        result_data = pl.DataFrame()
+        # Create the time feature mapping
+        if isinstance(time_feature, str):
+            time_feature = {
+                feature_name: time_feature
+                for feature_name in data.columns
+                if feature_name != time_feature
+            }
+
+        # Convert each feature into a time series
+        for value_feature, t_feature in time_feature.items():
+            result_data = result_data.with_columns(
+                data.select(
+                    [
+                        t_feature,
+                        value_feature,
+                    ]
+                )
+                .rename(
+                    {
+                        t_feature: "time",
+                        value_feature: "value",
+                    }
+                )
+                .to_struct()
+                .implode()
+                .alias(value_feature)
+            )
+
+        return Dataset(result_data)
