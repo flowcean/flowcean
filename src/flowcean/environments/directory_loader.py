d0be59b854a9099379dbb539068756c0c0499f1c
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-08-01 07:50:07 +0200

    Add docstring to `load_directory`

--- src/flowcean/environments/directory_loader.py
+++ src/flowcean/environments/directory_loader.py
@@ -7,11 +7,28 @@ from flowcean.core.environment.offline import OfflineEnvironment
 def load_directory(
     path: Path,
     load_function: Callable[[Path], OfflineEnvironment],
-    pattern: str = "*",
     *,
+    pattern: str = "*",
     include_files: bool = True,
     include_folders: bool = True,
 ) -> Iterable[OfflineEnvironment]:
+    """Load environments from a directory.
+
+    This helper function can be used to load environments from multiple files
+    or folders. First all files and folders in the `path` matching the
+    `pattern` are selected. These are then passed to the `load_function`
+    which in turn creates the actual environment.
+
+    Args:
+        path: The path to the directory from which environments are loaded.
+        load_function: Function handle to a function loading an environment
+            from a path and returning it.
+        pattern: A glob pattern. Matching files and folders will be passed to
+            `load_function`.
+        include_files: Specify whether to pass files to the `load_function`.
+        include_folders: Specify whether to pass folders to the
+            `load_function`.
+    """
     for item in path.glob(pattern):
         if (item.is_file() and include_files) or (
             item.is_dir() and include_folders
