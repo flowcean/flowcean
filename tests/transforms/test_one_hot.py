d6fff2efde3ae7054a85d2d224d1f334dd1b4798
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-08-01 08:50:05 +0200

    Add test for one-hot transform

---
+++ tests/transforms/test_one_hot.py
@@ -0,0 +1,83 @@
+import unittest
+
+import polars as pl
+from polars.testing import assert_frame_equal
+
+from flowcean.transforms import OneHot
+
+
+class OneHotTransform(unittest.TestCase):
+    def test_single(self) -> None:
+        transform = OneHot(["a"])
+
+        data_frame = pl.DataFrame(
+            [
+                {"a": 1, "b": 2, "c": 3},
+                {"a": 4, "b": 5, "c": 6},
+                {"a": 7, "b": 8, "c": 9},
+                {"a": 10, "b": 11, "c": 12},
+            ],
+        )
+        transformed_data = transform.transform(data_frame)
+
+        assert_frame_equal(
+            transformed_data,
+            pl.DataFrame(
+                {
+                    "a_1": [1, 0, 0, 0],
+                    "a_4": [0, 1, 0, 0],
+                    "a_7": [0, 0, 1, 0],
+                    "a_10": [0, 0, 0, 1],
+                    "b": [2, 5, 8, 11],
+                    "c": [3, 6, 9, 12],
+                },
+            ),
+            check_column_order=False,
+        )
+
+    def test_multiple(self) -> None:
+        transform = OneHot(["a", "b"])
+
+        data_frame = pl.DataFrame(
+            [
+                {"a": 1, "b": 2, "c": 3},
+                {"a": 4, "b": 5, "c": 6},
+                {"a": 7, "b": 8, "c": 9},
+                {"a": 10, "b": 11, "c": 12},
+            ],
+        )
+        transformed_data = transform.transform(data_frame)
+
+        assert_frame_equal(
+            transformed_data,
+            pl.DataFrame(
+                {
+                    "a_1": [1, 0, 0, 0],
+                    "a_4": [0, 1, 0, 0],
+                    "a_7": [0, 0, 1, 0],
+                    "a_10": [0, 0, 0, 1],
+                    "b_2": [1, 0, 0, 0],
+                    "b_5": [0, 1, 0, 0],
+                    "b_8": [0, 0, 1, 0],
+                    "b_11": [0, 0, 0, 1],
+                    "c": [3, 6, 9, 12],
+                },
+            ),
+            check_column_order=False,
+        )
+
+    def test_non_integer(self) -> None:
+        transform = OneHot(["a"])
+
+        data_frame = pl.DataFrame({"a": [1.456, 17.24, 42.0]})
+        transformed_data = transform.transform(data_frame)
+
+        assert_frame_equal(
+            transformed_data,
+            pl.DataFrame({"a": [1.456, 17.24, 42.0]}),
+            check_column_order=False,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
