ddb4f2630e40c99d4c884dd15668bbc4ac973ca3
Christian Wieck <167854903+ChristianWieck@users.noreply.github.com>
2024-08-06 12:15:05 +0200

    Update ALP example (#135)
    * Remove custom loader
    
    * Rewrote ALP example to use new flowcean features

--- examples/automatic_lashing_platform/run.py
+++ examples/automatic_lashing_platform/run.py
@@ -1,32 +1,63 @@
-from loader import AlpDataLoader
+import logging
+import time
+from pathlib import Path
+
+from tqdm import tqdm
 
 import flowcean.cli
+from flowcean.core.environment import StackEnvironment
+from flowcean.environments.json import JsonDataLoader
+from flowcean.environments.parquet import ParquetDataLoader
 from flowcean.environments.train_test_split import TrainTestSplit
 from flowcean.learners.regression_tree import RegressionTree
 from flowcean.metrics import MeanAbsoluteError, MeanSquaredError
 from flowcean.strategies.offline import evaluate_offline, learn_offline
-from flowcean.transforms.select import Select
+from flowcean.transforms import Flatten, Resample, Select
+
+logger = logging.getLogger(__name__)
 
 
 def main() -> None:
     flowcean.cli.initialize_logging()
-
-    data = AlpDataLoader(path="./data/").with_transform(
+    time_start = time.time()
+    data = StackEnvironment(
+        *[
+            ParquetDataLoader(path)
+            .load()
+            .to_time_series("t")
+            .join(JsonDataLoader(path.with_suffix(".json")).load())
+            for path in tqdm(
+                list(Path("./data").glob("*.parquet")),
+                desc="Loading environments",
+            )
+        ]
+    ).with_transform(
         Select(
             [
-                "^p_cylinder1_.*$",
-                "activeValveCount",
+                "p_accumulator",
                 "containerWeight",
-            ],
-        ),
+                "p_initial",
+                "activeValveCount",
+                "T",
+            ]
+        )
+        | Resample(1.0)
+        | Flatten()
     )
-    data.load()
+    time_end = time.time()
+    logger.info("took %.5f s to load data", time_end - time_start)
+
+    time_start = time.time()
     train, test = TrainTestSplit(ratio=0.8, shuffle=False).split(data)
+    time_end = time.time()
+    logger.info("took %.5f s to process data", time_end - time_start)
 
     learner = RegressionTree()
     inputs = [
-        "^p_cylinder1_.*$",
+        "^p_accumulator_.*$",
         "activeValveCount",
+        "p_initial",
+        "T",
     ]
     outputs = ["containerWeight"]
 
