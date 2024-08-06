76da4644af9b6d19555d1cedb13c4e4532b117d3
Maximilian Schmidt <maximilian@schmidt.so>
2024-08-06 11:14:35 +0200

    tqdm progress bar in env.collect() (#133)
    * tqdm progress bar in env.collect()
    
    * allow specifying keyword arguments
    
    * explicitly add tqdm as dependency

--- src/flowcean/core/environment/incremental.py
+++ src/flowcean/core/environment/incremental.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from collections.abc import Iterable
 from itertools import islice
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 import polars as pl
+from tqdm import tqdm
 
 if TYPE_CHECKING:
     from flowcean.environments.dataset import Dataset
@@ -20,19 +21,39 @@ class IncrementalEnvironment(Environment, Iterable[pl.DataFrame]):
     environment cannot be controlled.
     """
 
-    def collect(self, n: int) -> Dataset:
+    def collect(
+        self,
+        n: int,
+        *,
+        progress_bar: bool | dict[str, Any] = True,
+    ) -> Dataset:
         """Collect n samples.
 
         Args:
             n: Number of samples to collect.
+            progress_bar: Show a progress bar while collecting the samples. If
+                a dictionary is passed, it is used as keyword arguments for the
+                `tqdm` progress bar.
 
         Returns:
             Dataset with the collected data.
         """
         from flowcean.environments.dataset import Dataset
 
+        samples = islice(self, n)
+
+        if isinstance(progress_bar, dict):
+            progress_bar.setdefault("desc", "Collecting samples")
+            progress_bar.setdefault("total", n)
+            samples = tqdm(
+                samples,
+                **progress_bar,
+            )
+        elif progress_bar:
+            samples = tqdm(samples, desc="Collecting samples", total=n)
+
         data = pl.DataFrame()
-        for sample in islice(self, n):
+        for sample in samples:
             data = data.vstack(sample)
 
         return Dataset(data)
