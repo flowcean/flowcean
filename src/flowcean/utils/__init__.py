c02ff34b18b9f892f5ae1b3426077cca183ec17f
Christian Wieck <167854903+ChristianWieck@users.noreply.github.com>
2024-08-06 13:01:29 +0200

    Directory loader (#110)
    * Add helper method to load files from directory
    
    * Add static `loader` method to be used with `load_directory`
    
    * Add handling for bad environments
    
    * Add docstring to `load_directory`
    
    * Introduce new LoaderFunction type
    
    * Implement loader functions for default offline environments
    
    * Allow string as path argument
    
    * Reworked expression to functional style
    
    * Revert "Implement loader functions for default offline environments"
    
    This reverts commit f2812ec41c419b86c1278cd6dee8f6150bfffc6f.
    
    * Revert "Add static `loader` method to be used with `load_directory`"
    
    This reverts commit f082c28cebbee134efd8be05a141575fb3b3db9a.
    
    * Call `load` on created environments
    
    * Move `load_directory` to utils package
    
    * Remove `load` call
    
    * Renamed to build_environments_from_directory
    
    * Remove None filter
    
    * Replace list comprehension by map
    
    * Reworked expression to generator
    
    * Fix docstring

--- src/flowcean/utils/__init__.py
+++ src/flowcean/utils/__init__.py
@@ -1,5 +1,9 @@
 __all__ = [
+    "build_environments_from_directory",
     "is_timeseries_feature",
 ]
 
+from .build_environments_from_directory import (
+    build_environments_from_directory,
+)
 from .is_time_series import is_timeseries_feature
