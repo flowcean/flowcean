33f150c3926d9fa592b4836d0f79106cae4af698
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-08-15 11:12:50 +0200

    Add test for explicit categories

--- tests/transforms/test_one_hot.py
+++ tests/transforms/test_one_hot.py
@@ -64,6 +64,31 @@ class OneHotTransform(unittest.TestCase):
             check_column_order=False,
         )
 
+    def test_given_categories(self) -> None:
+        transform = OneHot({"a": [1, 7]})
+        data_frame = pl.DataFrame(
+            [
+                {"a": 1, "b": 2, "c": 3},
+                {"a": 4, "b": 5, "c": 6},
+                {"a": 7, "b": 8, "c": 9},
+                {"a": 10, "b": 11, "c": 12},
+            ],
+        )
+        transformed_data = transform.transform(data_frame)
+
+        assert_frame_equal(
+            transformed_data,
+            pl.DataFrame(
+                {
+                    "a_1": [1, 0, 0, 0],
+                    "a_7": [0, 0, 1, 0],
+                    "b": [2, 5, 8, 11],
+                    "c": [3, 6, 9, 12],
+                },
+            ),
+            check_column_order=False,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
