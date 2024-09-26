5813c1bf8e67d315eae87499757ced3fff89add0
Markus Knitt <markus.knitt@tuhh.de>
2024-09-26 11:07:20 +0200

    Set different random seeds for different libraries

--- src/flowcean/utils/random.py
+++ src/flowcean/utils/random.py
@@ -3,7 +3,6 @@ import random
 
 import numpy as np
 import polars as pl
-import torch
 
 rng = np.random.default_rng()
 
@@ -13,12 +12,18 @@ logger = logging.getLogger(__name__)
 def initialize_random(seed: int) -> None:
     global rng  # noqa: PLW0603
     rng = np.random.default_rng(seed)
-    random.seed(seed)
-    pl.set_random_seed(seed)
-    torch.manual_seed(0)
-    np.random.seed(seed)  # noqa: NPY002
+    random.seed(get_seed())
+    pl.set_random_seed(get_seed())
+
+    np.random.seed(get_seed())  # noqa: NPY002
+    try:
+        import torch
+
+        torch.manual_seed(get_seed())
+    except ModuleNotFoundError:
+        pass
     logger.info("Initialized random number generator with seed %d", seed)
 
 
 def get_seed() -> int:
-    return rng.integers(2**32 - 1)
+    return int(rng.integers(2**32 - 1))
