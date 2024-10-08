0a37abf022bd720515a3a87b657034608c17e45e
Markus Knitt <markus.knitt@tuhh.de>
2024-10-08 10:10:18 +0200

    Fix test

--- tests/data/test_train_test_split.py
+++ tests/data/test_train_test_split.py
@@ -28,10 +28,10 @@ class TestTrainTestSplit(unittest.TestCase):
         dataset = Dataset(pl.DataFrame({"a": [1, 2, 3, 4, 5, 6]}))
         train, test = TrainTestSplit(ratio=0.8, shuffle=True).split(dataset)
 
-        expected_train = pl.DataFrame({"a": [5, 6, 1, 2]})
+        expected_train = pl.DataFrame({"a": [2, 5, 1, 3]})
         assert_frame_equal(expected_train, train.get_data())
 
-        expected_test = pl.DataFrame({"a": [3, 4]})
+        expected_test = pl.DataFrame({"a": [6, 4]})
         assert_frame_equal(expected_test, test.get_data())
 
 
