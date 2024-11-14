7b46bc2705e6922c9e867f4e38a85b7b9b82d4c6
Christian Wieck <167854903+ChristianWieck@users.noreply.github.com>
2024-11-14 11:58:29 +0100

    Add Time series filter (#129)
    * Implement high- and lowpass filter
    
    * Add test for high- and lowpass filter
    
    * Fixed comment
    
    * Fixed variable names
    
    * Fix import path
    
    * Fix old calling syntax
    
    * Rename from Filter to SignalFilter

--- src/flowcean/transforms/__init__.py
+++ src/flowcean/transforms/__init__.py
@@ -8,6 +8,8 @@ __all__ = [
     "Rename",
     "Resample",
     "Select",
+    "SignalFilter",
+    "SignalFilterType",
     "SlidingWindow",
     "Standardize",
     "TimeWindow",
@@ -25,6 +27,10 @@ from flowcean.transforms.rechunk import Rechunk
 from flowcean.transforms.rename import Rename
 from flowcean.transforms.resample import Resample
 from flowcean.transforms.select import Select
+from flowcean.transforms.signal_filter import (
+    SignalFilter,
+    SignalFilterType,
+)
 from flowcean.transforms.sliding_window import SlidingWindow
 from flowcean.transforms.standardize import Standardize
 from flowcean.transforms.time_window import TimeWindow
