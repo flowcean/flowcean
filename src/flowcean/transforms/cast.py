06c405b6e225d46052b774921845f3dc13349cd8
Christian Wieck <167854903+ChristianWieck@users.noreply.github.com>
2024-11-21 11:24:47 +0100

    Add cast transform with test (#177)

---
+++ src/flowcean/transforms/cast.py
@@ -0,0 +1,40 @@
+import logging
+from collections.abc import Iterable
+from typing import override
+
+import polars as pl
+from polars._typing import PolarsDataType
+
+from flowcean.core.transform import Transform
+
+logger = logging.getLogger(__name__)
+
+
+class Cast(Transform):
+    """Cast features to a different datatype."""
+
+    def __init__(
+        self,
+        target_type: PolarsDataType,
+        *,
+        features: Iterable[str] | None = None,
+    ) -> None:
+        """Initializes the Cast transform.
+
+        Args:
+            target_type: Type to which the features will be cast.
+            features: The features to cast. If `None` all
+                features will be cast. This is the default behaviour.
+        """
+        self.target_type = target_type
+        self.features = features
+
+    @override
+    def apply(self, data: pl.DataFrame) -> pl.DataFrame:
+        return data.with_columns(
+            (
+                pl.all()
+                if self.features is None
+                else data.select(self.features)
+            ).cast(self.target_type)
+        )
