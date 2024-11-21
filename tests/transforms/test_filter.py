7d7b661647f82234b1d1bf808ce0f6cd51c0c445
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2024-11-21 11:50:32 +0100

    Replace filter_mode with class based system

--- tests/transforms/test_filter.py
+++ tests/transforms/test_filter.py
@@ -4,6 +4,7 @@ import polars as pl
 from polars.testing import assert_frame_equal
 
 from flowcean.transforms import Filter
+from flowcean.transforms.filter import And, Not, Or
 
 
 class FilterTransform(unittest.TestCase):
@@ -30,7 +31,7 @@ class FilterTransform(unittest.TestCase):
         )
 
     def test_multiple_filter_and(self) -> None:
-        transform = Filter(["(a > 2)", "b != 3"])
+        transform = Filter(And(["(a > 2)", "b != 3"]))
 
         data_frame = pl.DataFrame(
             {
@@ -52,7 +53,7 @@ class FilterTransform(unittest.TestCase):
         )
 
     def test_multiple_filter_or(self) -> None:
-        transform = Filter(["(a < 2)", "b < 2"], filter_mode="or")
+        transform = Filter(Or(["(a < 2)", "b < 2"]))
 
         data_frame = pl.DataFrame(
             {
@@ -68,6 +69,42 @@ class FilterTransform(unittest.TestCase):
                 {
                     "a": [1, 5],
                     "b": [5, 1],
+                },
+            ),
+            check_column_order=False,
+        )
+
+    def test_nested_expr(self) -> None:
+        transform = Filter(
+            And(
+                [
+                    "a > 2",
+                    Not(
+                        Or(
+                            [
+                                "a == 4",
+                                "b == 2",
+                            ]
+                        )
+                    ),
+                ]
+            ),
+        )
+
+        data_frame = pl.DataFrame(
+            {
+                "a": [1, 2, 3, 4, 5],
+                "b": [5, 4, 3, 2, 1],
+            },
+        )
+        transformed_data = transform(data_frame)
+
+        assert_frame_equal(
+            transformed_data,
+            pl.DataFrame(
+                {
+                    "a": [3, 5],
+                    "b": [3, 1],
                 },
             ),
             check_column_order=False,
