2c4add8794519a3281ce15c0199a42a96426f796
Markus Knitt <markus.knitt@tuhh.de>
2024-11-27 14:36:05 +0100

    Fix __init__.py

--- src/flowcean/transforms/__init__.py
+++ src/flowcean/transforms/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
+    "Cast",
     "Explode",
     "FeatureLengthVaryError",
     "FeatureNotFoundError",
     "Flatten",
     "NoTimeSeriesFeatureError",
+    "Lambda",
     "MatchSamplingRate",
     "NoCategoriesError",
     "NoMatchingCategoryError",
@@ -20,12 +22,14 @@ __all__ = [
     "ToTimeSeries",
 ]
 
+from flowcean.transforms.cast import Cast
 from flowcean.transforms.explode import Explode
 from flowcean.transforms.flatten import (
     FeatureLengthVaryError,
     Flatten,
     NoTimeSeriesFeatureError,
 )
+from flowcean.transforms.function import Lambda
 from flowcean.transforms.match_sampling_rate import (
     FeatureNotFoundError,
     MatchSamplingRate,
@@ -39,7 +43,10 @@ from flowcean.transforms.rechunk import Rechunk
 from flowcean.transforms.rename import Rename
 from flowcean.transforms.resample import Resample
 from flowcean.transforms.select import Select
-from flowcean.transforms.signal_filter import SignalFilter, SignalFilterType
+from flowcean.transforms.signal_filter import (
+    SignalFilter,
+    SignalFilterType,
+)
 from flowcean.transforms.sliding_window import SlidingWindow
 from flowcean.transforms.standardize import Standardize
 from flowcean.transforms.time_window import TimeWindow
