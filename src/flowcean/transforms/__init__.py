a7046cfb9a53dbcd67f8c5b02495a890e686e9f3
Markus Knitt <markus.knitt@tuhh.de>
2024-11-27 14:16:36 +0100

    Add match_sampling_rate and its test after hard reset on main

--- src/flowcean/transforms/__init__.py
+++ src/flowcean/transforms/__init__.py
@@ -1,10 +1,9 @@
 __all__ = [
-    "Cast",
     "Explode",
     "FeatureLengthVaryError",
+    "FeatureNotFoundError",
     "Flatten",
     "NoTimeSeriesFeatureError",
-    "Lambda",
     "MatchSamplingRate",
     "NoCategoriesError",
     "NoMatchingCategoryError",
@@ -21,15 +20,16 @@ __all__ = [
     "ToTimeSeries",
 ]
 
-from flowcean.transforms.cast import Cast
 from flowcean.transforms.explode import Explode
 from flowcean.transforms.flatten import (
     FeatureLengthVaryError,
     Flatten,
     NoTimeSeriesFeatureError,
 )
-from flowcean.transforms.function import Lambda
-from flowcean.transforms.match_sampling_rate import MatchSamplingRate
+from flowcean.transforms.match_sampling_rate import (
+    FeatureNotFoundError,
+    MatchSamplingRate,
+)
 from flowcean.transforms.one_hot import (
     NoCategoriesError,
     NoMatchingCategoryError,
@@ -39,10 +39,7 @@ from flowcean.transforms.rechunk import Rechunk
 from flowcean.transforms.rename import Rename
 from flowcean.transforms.resample import Resample
 from flowcean.transforms.select import Select
-from flowcean.transforms.signal_filter import (
-    SignalFilter,
-    SignalFilterType,
-)
+from flowcean.transforms.signal_filter import SignalFilter, SignalFilterType
 from flowcean.transforms.sliding_window import SlidingWindow
 from flowcean.transforms.standardize import Standardize
 from flowcean.transforms.time_window import TimeWindow
