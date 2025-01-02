219a95208293b7ff219903af08d0937891cc994a
Jannes Wegner <jannes.wegner@tuhh.de>
2025-01-02 16:59:44 +0100

    Add tools for inspection of the training-data
    interactive plot added

--- examples/automatic_lashing_platform/run.py
+++ examples/automatic_lashing_platform/run.py
@@ -11,7 +11,7 @@
 import logging
 import time
 import argparse
-import code
+import math
 
 # flowcean libraries
 import flowcean.cli
@@ -33,9 +33,12 @@ logger = logging.getLogger(__name__)
 
 # method to tain the model for the automatic lashing platform
 def main(args) -> None:
+    flowcean.cli.initialize_logging(parse_arguments=False)
+
+
     # load and prepare training data
 
-    flowcean.cli.initialize_logging(parse_arguments=False)
+    logger.info("Loading data...")
     time_start = time.time()
     data = (
         ParquetDataLoader("./alp_sim_data.parquet")
@@ -48,11 +51,11 @@ def main(args) -> None:
                 "T",
             ]
         )
-        | Resample(1.0)
+        | Resample(args.sample_rate)
         | Flatten()
     )
     time_end = time.time()
-    logger.info("took %.5f s to load data", time_end - time_start)
+    logger.info("Took %.5f s to load data", time_end - time_start)
 
     inputs = [
         "^p_accumulator_.*$",
@@ -63,43 +66,87 @@ def main(args) -> None:
     outputs = ["containerWeight"]
 
 
-    # show the training-data
+    # observe data
+    logger.info("Observing data...")
+    observed_data = data.observe()
+
+
+    # print overview of the data
+
+    if args.print_overview:
+        logger.info("Data overview:")
+
+        print(observed_data)
+        
+    
+    # print data
 
     if args.print_data:
-        print(data.observe())
+        logger.info(f"Printing {args.prints} rows:")
+
+        dimension = observed_data.shape[0]
+        prints = args.prints if args.prints < dimension else dimension
+
+        for i, c in zip(range(0, dimension, int(dimension/prints)), range(prints)):
+            index = i if args.print_distributed else c
+            print(f"Weight: {round(observed_data.select('containerWeight').row(index)[0], 3)}, Index: {index}")
+            print(observed_data.select('^p_accumulator_.*$').row(index))
+
+
+    # print a row of the data interactively
+
+    if args.print_row:
+        logger.info(f"Printing rows interactively:")
+        while True:
+            row_index = input("Enter the row index to print or 'x' to quit: ")
+            if row_index == 'x':
+                break
+            print(f"Weight: {round(observed_data.select('containerWeight').row(int(row_index))[0], 3)}")
+            print(observed_data.select('^p_accumulator_.*$').row(int(row_index)))
+
+
+    # plot data
 
     if args.plot_data:
-        observed_data = data.observe()
+        logger.info(f"Plotting {args.plots} rows:")
 
-        # plot_data = {}
-        # for input in inputs:	
-        #     plot_data[input] = observed_data.select(input)
-        # for output in outputs:
-        #     plot_data[output] = observed_data.select(output)
-        
-        # plt.figure()
-        # for i in range(args.plots):
-        #     plt.subplot(133)
-        #     plt.plot()    
+        dimension = observed_data.shape[0]
+        plots = args.plots if args.plots < dimension else dimension
+        plot_rows = math.ceil(math.sqrt(plots))
+        plot_cols = math.ceil(plots / plot_rows)
 
-        # i = 1
-        # for row in observed_data.iter_rows():
-        #     print(row)     
-        #     if i > 9: break
-        #     i += 1
+        plt.figure()
+        for i, c in zip(range(0, dimension, int(dimension/plots)), range(plots)):
+            plt.subplot(plot_rows, plot_cols, c+1)
+            index = i if args.plot_distributed else c
+            plt.title(f"Weight: {round(observed_data.select('containerWeight').row(index)[0], 3)}, Index: {index}")
+            plt.plot(observed_data.select('^p_accumulator_.*$').row(index))
 
-        # TODO: plot the data
-        # code.interact(local=locals())
-        
+        plt.subplots_adjust(hspace=0.5, wspace=0.5, left= 0.05, right=0.95, top=0.95, bottom=0.05)
+        plt.show()
 
-        # for input in inputs:
-        #     plt.plot(observed_data[input], label=input)
-        # plt.show()
+
+    # plot a row of the data
+    if args.plot_row:
+        logger.info(f"Plotting rows interactively:")
+
+        plt.figure()
+        plt.subplots_adjust(hspace=0.5, wspace=0.5, left= 0.05, right=0.95, top=0.95, bottom=0.05)
+
+        while True:
+            row_index = input("Enter the row index to plot or 'x' to quit: ")
+            if row_index == 'x':
+                break
+            plt.title(f"Weight: {round(observed_data.select('containerWeight').row(int(row_index))[0], 3)}, Index: {row_index}")
+            plt.plot(observed_data.select('^p_accumulator_.*$').row(int(row_index)))
+            plt.show()
 
 
     # train the model
 
     if not args.no_training:
+        logger.info("Training the model:")
+
         train_env, test_env = TrainTestSplit(ratio=0.8, shuffle=False).split(data)
         time_end = time.time()
 
@@ -132,7 +179,11 @@ def main(args) -> None:
 
         print(report)
 
+
+        # plot the error of the trained model
+
         if args.plot_error:
+            # TODO
             pass
 
 
@@ -142,14 +193,34 @@ if __name__ == "__main__":
         prog="run.py",
         description="Run the Automatic Lashing Platform example.",
     )
-    parser.add_argument('--print_data', action='store_true', help='Print the data from the parquet file.')
-    parser.add_argument('--plot_data', action='store_true', help='Plot the data from the parquet file.')
-    parser.add_argument('--plots', type=int, default=10, help='Number of plots to show.')
+
+
+    # parameter
+
+    parser.add_argument('--sample_rate', type=float, default=1.0, help='Set the sample rate for the data. (default: 1.0) -> 15 Values')
+
+
+    # data inspection
+
+    parser.add_argument('--print_overview', action='store_true', help='Print a short overview ot the training-data.')
+    parser.add_argument('--print_data', action='store_true', help='Print a number of rows of the training-data.')
+    parser.add_argument('--prints', type=int, default=10, metavar='NUMBER', help='Number of rows to print. (default: 10)')
+    parser.add_argument('--print_distributed', action='store_true', help='Print from distributed training-data.')
+    parser.add_argument('--print_row', action='store_true', help='Print a row of the training-data interactively.')
+    parser.add_argument('--plot_data', action='store_true', help='Plot the training-data .')
+    parser.add_argument('--plots', type=int, default=10, metavar='NUMBER', help='Number of plots to show. (default: 10)')
+    parser.add_argument('--plot_distributed', action='store_true', help='Plot from distributed training-data.')
+    parser.add_argument('--plot_row', action='store_true', help='Plot a row of the training-data interactively.')
+
+
+    # training
+
     parser.add_argument('--no_training', action='store_true', help='Apply no training.')
     parser.add_argument('--lightning_learner', action='store_true', help='Use the Lightning Learner with Multilayer-Perceptron instead of Regression-Tree.')
-    parser.add_argument('--learning_rate', type=float, default=0.1, help='Set the learning rate for the lightning-model. (default: 0.1)')
+    parser.add_argument('--learning_rate', type=float, default=0.1, metavar='RATE', help='Set the learning rate for the lightning-model. (default: 0.1)')
     parser.add_argument('--plot_error', action='store_true', help='Plot the error of the trained model.')
     
+
     args = parser.parse_args()
 
     main(args)
