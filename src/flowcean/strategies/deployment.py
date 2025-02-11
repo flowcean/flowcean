be3fc13a5bef1d1644fcbb2a4b0a23cd0f355432
Sean Maroofi <sean.maroofi@tuhh.de>
2025-02-11 09:39:54 +0100

    removed unnecessary apply function, removed unused param in deploy function

--- src/flowcean/strategies/deployment.py
+++ src/flowcean/strategies/deployment.py
@@ -7,7 +7,6 @@ from flowcean.core.model import ModelWithTransform
 
 def deploy(
     environment: ActiveEnvironment | IncrementalEnvironment,
-    config: list[str],
     model: ModelWithTransform,
     input_transforms: Transform = Identity(),
     output_transforms: Transform  = Identity()
@@ -30,21 +29,3 @@ def deploy(
 
     if isinstance(environment, ActiveEnvironment):
         environment.act(output_transforms.apply(output).collect())
-
-def apply(
-    observation: pl.DataFrame,
-    model: ModelWithTransform,
-    input_transforms: Transform = Identity(),
-    output_transforms: Transform = Identity(),
-) -> pl.DataFrame:
-    """
-    Use a trained model
-
-    Args:
-        observation: an observation of the environments current state
-        model: the trained model
-        input_transforms: system specific transforms for model input
-        output_transforms: system specific transforms for system input
-    """
-    output = model.predict(input_transforms.apply(observation))
-    return output_transforms.apply(output)
