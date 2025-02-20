228fbf4b9a3c323a49d51987dcc933b0dc26585e
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2025-02-20 15:20:29 +0100

    Add replace flag

--- src/flowcean/polars/transforms/mean.py
+++ src/flowcean/polars/transforms/mean.py
@@ -12,23 +12,34 @@ logger = logging.getLogger(__name__)
 class Mean(Transform):
     """Replaces time-series features with their mean value."""
 
-    def __init__(self, features: str | Iterable[str]) -> None:
+    def __init__(
+        self,
+        features: str | Iterable[str],
+        *,
+        replace: bool = False,
+    ) -> None:
         """Initializes the Mean transform.
 
         Args:
             features: The feature or features the mean should be calculated
                 for.
+            replace: Whether to replace the original features with the
+                transformed ones. If set to False, the default, the value will
+                be added as a new feature named `{feature}_mean`.
         """
         self.features = [features] if isinstance(features, str) else features
+        self.replace = replace
 
     @override
     def apply(self, data: pl.LazyFrame) -> pl.LazyFrame:
         for feature in self.features:
-            data = data.with_columns(
+            expr = (
                 pl.col(feature)
                 .list.eval(pl.element().struct.field("value"))
                 .list.mean()
-                .alias(f"{feature}_mean"),
+            )
+            data = data.with_columns(
+                expr if self.replace else expr.alias(f"{feature}_mean"),
             )
 
         return data
