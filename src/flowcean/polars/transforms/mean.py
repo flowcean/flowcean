993adf014dcdf094dde17575b2954e2f36511925
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2025-02-20 14:04:00 +0100

    Add mean transform for time series data

---
+++ src/flowcean/polars/transforms/mean.py
@@ -0,0 +1,33 @@
+import logging
+from collections.abc import Iterable
+
+import polars as pl
+from typing_extensions import override
+
+from flowcean.core import Transform
+
+logger = logging.getLogger(__name__)
+
+
+class Mean(Transform):
+    """Replaces time-series features with there mean value."""
+
+    def __init__(self, features: str | Iterable[str]) -> None:
+        """Initializes the Mean transform.
+
+        Args:
+            features: The feature or features the mean should be calculated
+                for.
+        """
+        self.features = [features] if isinstance(features, str) else features
+
+    @override
+    def apply(self, data: pl.LazyFrame) -> pl.LazyFrame:
+        for feature in self.features:
+            data = data.with_columns(
+                pl.col(feature)
+                .list.eval(pl.element().struct.field("value"))
+                .list.mean(),
+            )
+
+        return data
