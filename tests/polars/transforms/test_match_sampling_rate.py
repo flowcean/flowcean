4aa8a9db513c8f0e24aad9fd9ff93b71a179ff4c
Markus Knitt <markus.knitt@tuhh.de>
2025-02-27 17:03:57 +0100

    Add test for scalar time series

--- tests/polars/transforms/test_match_sampling_rate.py
+++ tests/polars/transforms/test_match_sampling_rate.py
@@ -1462,6 +1462,108 @@ class TestMatchSamplingRate(unittest.TestCase):
         )
         assert_frame_equal(transformed_data, expected_data)
 
+    def test_scalar_timeseries_values(self) -> None:
+        transform = MatchSamplingRate(
+            reference_feature_name="feature_a",
+            feature_interpolation_map={
+                "feature_b": "linear",
+            },
+            fill_strategy="zero",
+        )
+
+        data_frame = pl.DataFrame(
+            {
+                "feature_a": [
+                    [
+                        {
+                            "time": 1.0,
+                            "value": 1.2,
+                        },
+                        {
+                            "time": 2.0,
+                            "value": 2.4,
+                        },
+                        {
+                            "time": 3.0,
+                            "value": 3.6,
+                        },
+                        {
+                            "time": 4.0,
+                            "value": 4.8,
+                        },
+                    ],
+                ],
+                "feature_b": [
+                    [
+                        {
+                            "time": 2.0,
+                            "value": 1.0,
+                        },
+                        {
+                            "time": 3.0,
+                            "value": 2.0,
+                        },
+                        {
+                            "time": 4.0,
+                            "value": 3.0,
+                        },
+                        {
+                            "time": 5.0,
+                            "value": 4.0,
+                        },
+                    ],
+                ],
+            },
+        )
+
+        transformed_data = transform(data_frame.lazy()).collect()
+
+        expected_data = pl.DataFrame(
+            {
+                "feature_a": [
+                    [
+                        {
+                            "time": 1.0,
+                            "value": 1.2,
+                        },
+                        {
+                            "time": 2.0,
+                            "value": 2.4,
+                        },
+                        {
+                            "time": 3.0,
+                            "value": 3.6,
+                        },
+                        {
+                            "time": 4.0,
+                            "value": 4.8,
+                        },
+                    ],
+                ],
+                "feature_b": [
+                    [
+                        {
+                            "time": 1.0,
+                            "value": 0.0,
+                        },
+                        {
+                            "time": 2.0,
+                            "value": 1.0,
+                        },
+                        {
+                            "time": 3.0,
+                            "value": 2.0,
+                        },
+                        {
+                            "time": 4.0,
+                            "value": 3.0,
+                        },
+                    ],
+                ],
+            },
+        )
+        assert_frame_equal(transformed_data, expected_data)
+
 
 if __name__ == "__main__":
     unittest.main()
