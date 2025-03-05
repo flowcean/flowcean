14b241fbb9cdaf3f7e2b1ecee5c1eb8283b68df6
Christian Wieck <167854903+ChristianWieck@users.noreply.github.com>
2025-03-05 12:00:38 +0100

    Cast dictionary argument (#235)
    * Add dictionary support for cast and test new argument option
    
    * Update docstring

--- src/flowcean/polars/transforms/cast.py
+++ src/flowcean/polars/transforms/cast.py
@@ -11,11 +11,39 @@ logger = logging.getLogger(__name__)
 
 
 class Cast(Transform):
-    """Cast features to a different datatype."""
+    """Cast features to a different datatype.
+
+    This transform allows to change the datatype of features in a DataFrame.
+    To cast all features to the same datatype, provide a single type as the
+    `target_type` argument e.g.
+
+    ```python
+    transform = Cast(pl.Float64)
+    ```
+
+    By specifying the `features` keyword argument, only the selected features
+    will be cast e.g.
+
+    ```python
+    transform = Cast(pl.Float64, features=["feature_a"])
+    ```
+
+    Lastly, to cast features to different datatypes, provide a dictionary with
+    feature names as keys and target types as values e.g.
+
+    ```python
+    transform = Cast(
+        {
+            "feature_a": pl.Boolean,
+            "feature_b": pl.Float64,
+        },
+    )
+    ```
+    """
 
     def __init__(
         self,
-        target_type: PolarsDataType,
+        target_type: PolarsDataType | dict[str, PolarsDataType],
         *,
         features: Iterable[str] | None = None,
     ) -> None:
@@ -23,6 +51,10 @@ class Cast(Transform):
 
         Args:
             target_type: Type to which the features will be cast.
+                If a single type is provided, all features or those provided in
+                the `features` keyword argument will be cast to that specific
+                type. To cast features to different types, provide a dictionary
+                with feature names as keys and target types as values.
             features: The features to cast. If `None` all
                 features will be cast. This is the default behaviour.
         """
@@ -31,6 +63,12 @@ class Cast(Transform):
 
     @override
     def apply(self, data: pl.LazyFrame) -> pl.LazyFrame:
+        if isinstance(self.target_type, dict):
+            for feature, target_type in self.target_type.items():
+                data = data.with_columns(
+                    pl.col(feature).cast(target_type),
+                )
+            return data
         return data.with_columns(
             (
                 pl.all() if self.features is None else pl.col(self.features)
