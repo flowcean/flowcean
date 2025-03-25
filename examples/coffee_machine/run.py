51a53b2eabc1ef1a5246c50a5e04dd7a968a1163
Maximilian Schmidt <maximilian@schmidt.so>
2025-03-25 13:02:51 +0100

    Restructure Examples into Packages (#233)
    * Remove unused workspace members
    
    * Restructure examples into python packages
    
    * Sync all packages in the workspace for pyright check

--- examples/coffee_machine/run.py
+++ examples/coffee_machine/run.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
-# /// script
-# dependencies = [
-#     "flowcean",
-# ]
-#
-# [tool.uv.sources]
-# flowcean = { path = "../../", editable = true }
-# ///
 
 from pathlib import Path
 
