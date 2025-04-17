1cb7bee22a4ea5b2887f4c90b495506f24e8a53e
Maximilian Schmidt <maximilian@schmidt.so>
2025-04-17 11:07:21 +0200

    Revert "some version with index based joining"
    This reverts commit ca90be6ecfef92974176b94a622f7cc18fb6a9a5.

--- examples/robot_localization_failure/exp.py
+++ examples/robot_localization_failure/exp.py
@@ -1,5 +1,3 @@
-from collections.abc import Iterable
-
 import polars as pl
 from run import load_or_cache_ros_data
 
@@ -7,12 +5,11 @@ data = load_or_cache_ros_data()
 
 topics = [
     "/amcl_pose",
-    # "/momo/pose",
-    # "/scan",
-    "/map",
-    # "/particle_cloud",
-    # not really inputs:
-    # "/delocalizations",
+    "/momo/pose",
+    "/scan",
+    # "/map",
+    "/delocalizations",
+    "/particle_cloud",
     # "/position_error",
     # "/heading_error",
 ]
@@ -20,47 +17,25 @@ topics = [
 data_multiple = pl.concat([data] * 2)
 
 
-# def explode_timeseries_expr(columns: list[str]) -> Iterable[pl.Expr]:
-#     for column in columns:
-#         yield pl.col(column).explode().struct.unnest()
-
-
 def explode_timeseries(df: pl.LazyFrame, column: str) -> pl.LazyFrame:
     exploded = df.select(column).with_row_index().explode(column)
     unnested = exploded.unnest(column)
     renamed = unnested.with_columns(
-        pl.col("value").name.prefix_fields(f"{column}/"),
+        pl.col("value").name.map_fields(lambda x: f"{column}/{x}"),
     )
-    unnested_2 = renamed.unnest("value")
-    return unnested_2.with_row_index(f"{column}_i")
+    return renamed.unnest("value")
 
 
 def join_topics(df: pl.LazyFrame, topics: list[str]) -> pl.LazyFrame:
     it = iter(topics)
-    topic = next(it)
-    joined = explode_timeseries(df, topic).select(
-        ["index", "time", f"{topic}_i"],
-    )
+    joined = explode_timeseries(df, next(it))
 
     for topic in it:
         joined = joined.join(
-            explode_timeseries(df, topic).select(
-                ["index", "time", f"{topic}_i"],
-            ),
+            explode_timeseries(df, topic),
             on=["index", "time"],
             how="full",
             coalesce=True,
-        )
-
-    return joined.sort(["index", "time"])
-
-
-def join_topics_2(df: pl.LazyFrame, topics: list[str]) -> pl.LazyFrame:
-    joined = df
-    for topic in topics:
-        joined = joined.join(
-            explode_timeseries(data_multiple, topic).drop(["index", "time"]),
-            on=f"{topic}_i",
         )
 
     return joined.sort(["index", "time"])
@@ -70,4 +45,3 @@ joined = join_topics(data_multiple, topics)
 filled = joined.select(
     pl.all().forward_fill().over("index"),
 ).drop_nulls()
-filled_full = join_topics_2(filled, topics)
