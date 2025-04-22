f0e6dc9a7eb4381f6b656c284dc197be01a831d6
Maximilian Schmidt <maximilian@schmidt.so>
2025-04-22 13:19:49 +0200

    refactored and documented slicing

--- examples/robot_localization_failure/slice_ts.py
+++ examples/robot_localization_failure/slice_ts.py
@@ -2,6 +2,68 @@ from collections.abc import Callable
 
 import polars as pl
 
+
+def slice_time_series(
+    time_series: pl.Expr,
+    slices: pl.Expr,
+) -> pl.Expr:
+    """Slice a list-of-structs time series into multiple segments.
+
+    For each struct in `slices` (with fields `from` and `to`), this function
+    extracts all samples from `time_series` (structs with `time` and `value`)
+    whose `time` lies in `[from, to)`.
+
+    Args:
+        time_series: Expression of a list column containing structs with fields
+            `time` and `value`.
+        slices: Expression of a list column containing structs with fields
+            `from` and `to`.
+
+    Returns:
+        A list-of-lists expression: for each row, an outer list over slices,
+        each element is the sub-series (a list of structs) for that slice.
+    """
+
+    def _list_eval(
+        list_col: pl.Expr | str,
+        func: Callable[..., pl.Expr],
+        *refs: pl.Expr | str,
+    ) -> pl.Expr:
+        """Evaluate `func` on elements of a list column, with references."""
+        elements = pl.element().struct[0].explode()
+        references = [pl.element().struct[i + 1] for i in range(len(refs))]
+        return pl.concat_list(pl.struct(list_col, *refs)).list.eval(
+            func(elements, *references),
+        )
+
+    def _filter_between(
+        sample: pl.Expr,
+        lower: pl.Expr,
+        upper: pl.Expr,
+    ) -> pl.Expr:
+        return sample.filter(
+            sample.struct.field("time").is_between(
+                lower,
+                upper,
+                closed="left",
+            ),
+        )
+
+    return _list_eval(
+        slices,
+        lambda slice_range, series: _list_eval(
+            series,
+            lambda sample, slice_range: _filter_between(
+                sample,
+                slice_range.struct.field("from"),
+                slice_range.struct.field("to"),
+            ),
+            slice_range,
+        ),
+        time_series,
+    )
+
+
 df = pl.DataFrame(
     {
         "a": [[{"time": i * 2, "value": i * 10} for i in range(100_000)]],
@@ -17,49 +79,8 @@ df = pl.DataFrame(
         "map": [True],
     },
 )
-
-
-def slice_time_series(time_series: pl.Expr, slices: pl.Expr) -> pl.Expr:
-    def list_eval_with(
-        listcol: pl.Expr | str,
-        operation: Callable[..., pl.Expr],
-        *ref_cols: str | pl.Expr,
-    ) -> pl.Expr:
-        arguments = [pl.element().struct[0].explode()] + [
-            pl.element().struct[i + 1] for i in range(len(ref_cols))
-        ]
-        return pl.concat_list(pl.struct(listcol, *ref_cols)).list.eval(
-            operation(*arguments),
-        )
-
-    def is_between(
-        element: pl.Expr,
-        lower: pl.Expr,
-        upper: pl.Expr,
-    ) -> pl.Expr:
-        return element.filter(
-            element.struct.field("time").is_between(
-                lower,
-                upper,
-                closed="left",
-            ),
-        )
-
-    return list_eval_with(
-        slices,
-        lambda from_to, time_series: list_eval_with(
-            time_series,
-            lambda sample, from_to: is_between(
-                sample,
-                from_to.struct.field("from"),
-                from_to.struct.field("to"),
-            ),
-            from_to,
-        ),
-        time_series,
-    )
-
-
-after = df.sample(2, with_replacement=True).with_columns(
-    slice_time_series(pl.col("a"), pl.col("slices")).alias("a"),
+df = df.with_columns(
+    a_sliced=slice_time_series(pl.col("a"), pl.col("slices")).alias(
+        "a_sliced",
+    ),
 )
