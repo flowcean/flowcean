bce28b7f5b3b522ffe9d298527eb4b04b845419b
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2025-04-24 07:23:21 +0200

    Streamline test for xor example

--- examples/xor/test.py
+++ examples/xor/test.py
@@ -4,11 +4,7 @@ import polars as pl
 
 from flowcean.core.model import Model
 from flowcean.core.tool import test_model
-from flowcean.core.tool.testing.generator import (
-    StochasticDataFrameGenerator,
-)
-from flowcean.core.tool.testing.generator.range import Uniform
-from flowcean.core.tool.testing.predicates import NotPredicate, PolarsPredicate
+from flowcean.core.tool.testing.predicates import PolarsPredicate
 from flowcean.polars.environments.dataframe import DataFrame
 from flowcean.polars.transforms.select import Select
 
@@ -24,15 +20,6 @@ def main() -> None:
         .to_incremental()
     )
 
-    test_generator = StochasticDataFrameGenerator(
-        [
-            # Generate random values for x and y
-            Uniform("x", 0, 1),
-            Uniform("y", 0, 1),
-        ],
-        seed=42,
-    )
-
     # Define a predicate to check with the data
     predicate = PolarsPredicate(
         pl.col("x").xor(pl.col("y")) == pl.col("z"),
@@ -41,11 +28,10 @@ def main() -> None:
     # Test the model with the test data and predicate
     test_model(
         model,
-        # test_data,
-        test_generator,
+        test_data,
         predicate,
         show_progress=True,
-        stop_after=10,
+        stop_after=0,
     )
 
 
