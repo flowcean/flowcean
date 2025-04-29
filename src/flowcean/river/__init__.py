6aa10274bc319f7b2eba25ee45062c2fe1fb0015
Lena Zukunft <lena.zukunft@tuhh.de>
2025-04-29 14:21:35 +0200

    remove traintestsplit

--- src/flowcean/river/__init__.py
+++ src/flowcean/river/__init__.py
@@ -1,9 +1,6 @@
-from flowcean.river.train_test_split import TrainTestSplit
-
 from .river_learner import RiverLearner, RiverModel
 
 __all__ = [
     "RiverLearner",
     "RiverModel",
-    "TrainTestSplit",
 ]
