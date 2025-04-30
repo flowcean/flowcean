8c25463d336b9224712dd3d6fa15ba61d05ef0dd
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2025-04-30 11:05:55 +0200

    Add Discrete range to exported classes

--- src/flowcean/core/tool/testing/generator/range/__init__.py
+++ src/flowcean/core/tool/testing/generator/range/__init__.py
@@ -1,9 +1,11 @@
 __all__ = [
+    "Discrete",
     "Fixed",
     "Range",
     "Uniform",
 ]
 
+from .discrete import Discrete
 from .fixed import Fixed
 from .range import Range
 from .uniform import Uniform
