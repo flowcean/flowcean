4da5d3b8fa799a9516822a1222e307a0905de3ec
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2025-04-30 10:05:03 +0200

    Add test for uniform range

---
+++ tests/tools/testing/range/test_uniform.py
@@ -0,0 +1,41 @@
+import unittest
+
+from flowcean.core.tool.testing.generator.range import Uniform
+
+
+class TestUniformRange(unittest.TestCase):
+    def test_sampling(self) -> None:
+        r = Uniform("feature", 0, 10)
+        r.set_seed(0)
+
+        sample = r()
+
+        assert 0 <= sample <= 10, f"Sample {sample} is out of range [0, 10]"
+
+    def test_to_discrete(self) -> None:
+        r = Uniform("feature", 0, 10)
+        r.set_seed(0)
+
+        discrete = r.to_discrete(1)
+
+        assert discrete.values[0] == 0
+        assert discrete.values[-1] == 10
+
+        assert len(discrete.values) == 11, (
+            "Discrete range should have 11 values"
+        )
+
+    def test_to_discrete_odd(self) -> None:
+        r = Uniform("feature", 0, 10)
+        r.set_seed(0)
+
+        discrete = r.to_discrete(3)
+
+        assert discrete.values[0] == 0
+        assert discrete.values[-1] == 9
+
+        assert len(discrete.values) == 4, "Discrete range should have 4 values"
+
+
+if __name__ == "__main__":
+    unittest.main()
