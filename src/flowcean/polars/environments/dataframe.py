d6d3dbe061ca3347aefa181f91afce4259295b94
Christian Wieck <167854903+ChristianWieck@users.noreply.github.com>
2025-04-30 11:22:58 +0200

    Add Environment hashing (#252)
    * Introduce hash abstract method for OfflineEnvironments
    
    * First implementation of hashing for DataFrames
    
    * Add tests for dataframe hashing
    
    * Implement hashing for JoinedEnvironments
    
    * Lazy hash dataframes
    
    * Fix old streaming argument

--- src/flowcean/polars/environments/dataframe.py
+++ src/flowcean/polars/environments/dataframe.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
-from collections.abc import Collection, Iterable
+import hashlib
+from collections.abc import Collection, Generator, Iterable
 from itertools import islice
 from pathlib import Path
 from typing import Any, cast
 from urllib.parse import urlparse
 
+import cloudpickle
 import polars as pl
 from ruamel.yaml import YAML
 from tqdm import tqdm
@@ -24,19 +26,29 @@ class DataFrame(OfflineEnvironment):
     """
 
     data: pl.LazyFrame
+    _hash: bytes | None = None
     _length: int | None = None
 
-    def __init__(self, data: pl.DataFrame | pl.LazyFrame) -> None:
+    def __init__(
+        self,
+        data: pl.DataFrame | pl.LazyFrame,
+        *,
+        data_hash: bytes | None = None,
+    ) -> None:
         """Initialize the dataset environment.
 
         Args:
             data: The data to represent.
+            data_hash: The hash of the data. If None, it will be computed from
+                the dataframe which is potentially slow and expensive.
         """
         if isinstance(data, pl.DataFrame):
             self.data = data.lazy()
             self._length = len(data)
         else:
             self.data = data
+
+        self._hash = data_hash
         super().__init__()
 
     @classmethod
@@ -49,7 +61,7 @@ class DataFrame(OfflineEnvironment):
         """
         data = pl.scan_csv(path, separator=separator)
         data = data.rename(lambda column_name: column_name.strip())
-        return cls(data)
+        return cls(data, data_hash=_hash_from_path(path))
 
     @classmethod
     def from_json(cls, path: str | Path) -> Self:
@@ -59,7 +71,7 @@ class DataFrame(OfflineEnvironment):
             path: Path to the JSON file.
         """
         data = pl.read_json(path)
-        return cls(data)
+        return cls(data, data_hash=_hash_from_path(path))
 
     @classmethod
     def from_parquet(cls, path: str | Path) -> Self:
@@ -69,7 +81,7 @@ class DataFrame(OfflineEnvironment):
             path: Path to the Parquet file.
         """
         data = pl.scan_parquet(path)
-        return cls(data)
+        return cls(data, data_hash=_hash_from_path(path))
 
     @classmethod
     def from_yaml(cls, path: str | Path) -> Self:
@@ -79,7 +91,7 @@ class DataFrame(OfflineEnvironment):
             path: Path to the YAML file.
         """
         data = pl.LazyFrame(YAML(typ="safe").load(path))
-        return cls(data)
+        return cls(data, data_hash=_hash_from_path(path))
 
     @classmethod
     def from_uri(cls, uri: str) -> Self:
@@ -106,6 +118,12 @@ class DataFrame(OfflineEnvironment):
     def _observe(self) -> pl.LazyFrame:
         return self.data
 
+    @override
+    def hash(self) -> bytes:
+        if self._hash is None:
+            self._hash = _hash_from_dataframe(self.data)
+        return self._hash
+
     def __len__(self) -> int:
         """Return the number of samples in the dataset."""
         if self._length is None:
@@ -122,6 +140,52 @@ def _file_uri_to_path(uri: str) -> Path:
     if url.scheme != "file":
         raise InvalidUriSchemeError(url.scheme)
     return Path(url.path)
+
+
+def _hash_from_dataframe(data: pl.LazyFrame) -> bytes:
+    hasher = hashlib.sha256()
+    schema = data.collect_schema()
+    hasher.update(cloudpickle.dumps(schema))
+
+    for batch in _iter_slices(data, 42):
+        # This is a workaround for the fact that Polars does not support
+        # hashing DataFrames directly
+        hasher.update(batch.to_numpy().dumps())
+    return hasher.digest()
+
+
+def _hash_from_path(path: str | Path) -> bytes:
+    hasher = hashlib.sha256()
+    with Path(path).open("rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            hasher.update(chunk)
+    return hasher.digest()
+
+
+def _iter_slices(
+    df: pl.LazyFrame,
+    batch_size: int,
+) -> Generator[pl.DataFrame, Any, None]:
+    def get_batch(
+        df: pl.LazyFrame,
+        offset: int,
+        batch_size: int,
+    ) -> pl.DataFrame:
+        return df.slice(offset, batch_size).collect(engine="streaming")
+
+    batch = get_batch(df, 0, batch_size)
+    # Yield once even if we got passed an empty LazyFrame
+    yield batch
+    offset = len(batch)
+    if offset:
+        while True:
+            batch = get_batch(df, offset, batch_size)
+            len_ = len(batch)
+            if len_:
+                offset += len_
+                yield batch
+            else:
+                break
 
 
 class InvalidUriSchemeError(Exception):
