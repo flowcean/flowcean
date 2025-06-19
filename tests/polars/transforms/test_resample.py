6e830c7c179df6b91e380f0450311b037d7a7af7
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2025-06-19 13:10:40 +0200

    Use pl.Time for time features

--- tests/polars/transforms/test_resample.py
+++ tests/polars/transforms/test_resample.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import time
 
 import polars as pl
 from polars.testing import assert_frame_equal
@@ -14,21 +15,21 @@ class ResampleTransform(unittest.TestCase):
                 "feature_a": [
                     [
                         {
-                            "time": 0,
+                            "time": time(second=0),
                             "value": 1,
                         },
                         {
-                            "time": 2,
+                            "time": time(second=2),
                             "value": 2,
                         },
                     ],
                     [
                         {
-                            "time": 0,
+                            "time": time(second=0),
                             "value": 0,
                         },
                         {
-                            "time": 3,
+                            "time": time(second=3),
                             "value": 3,
                         },
                     ],
@@ -38,47 +39,50 @@ class ResampleTransform(unittest.TestCase):
         )
         transformed_data = transform(data_frame.lazy()).collect()
 
+        expected_data = pl.DataFrame(
+            {
+                "feature_a": [
+                    [
+                        {
+                            "time": time(second=0),
+                            "value": 1.0,
+                        },
+                        {
+                            "time": time(second=1),
+                            "value": 1.5,
+                        },
+                        {
+                            "time": time(second=2),
+                            "value": 2.0,
+                        },
+                    ],
+                    [
+                        {
+                            "time": time(second=0),
+                            "value": 0.0,
+                        },
+                        {
+                            "time": time(second=1),
+                            "value": 1.0,
+                        },
+                        {
+                            "time": time(second=2),
+                            "value": 2.0,
+                        },
+                        {
+                            "time": time(second=3),
+                            "value": 3.0,
+                        },
+                    ],
+                ],
+                "scalar": [1, 2],
+            },
+        )
+
         assert_frame_equal(
             transformed_data,
-            pl.DataFrame(
-                {
-                    "feature_a": [
-                        [
-                            {
-                                "time": 0.0,
-                                "value": 1.0,
-                            },
-                            {
-                                "time": 1.0,
-                                "value": 1.5,
-                            },
-                            {
-                                "time": 2.0,
-                                "value": 2.0,
-                            },
-                        ],
-                        [
-                            {
-                                "time": 0.0,
-                                "value": 0.0,
-                            },
-                            {
-                                "time": 1.0,
-                                "value": 1.0,
-                            },
-                            {
-                                "time": 2.0,
-                                "value": 2.0,
-                            },
-                            {
-                                "time": 3.0,
-                                "value": 3.0,
-                            },
-                        ],
-                    ],
-                    "scalar": [1, 2],
-                },
-            ),
+            expected_data,
+            check_column_order=False,
         )
 
     def test_different_sample_rates(self) -> None:
@@ -88,11 +92,11 @@ class ResampleTransform(unittest.TestCase):
                 "feature_a": [
                     [
                         {
-                            "time": 0,
+                            "time": time(second=0),
                             "value": 1,
                         },
                         {
-                            "time": 2,
+                            "time": time(second=2),
                             "value": 2,
                         },
                     ],
@@ -100,15 +104,15 @@ class ResampleTransform(unittest.TestCase):
                 "feature_b": [
                     [
                         {
-                            "time": 0,
+                            "time": time(second=0),
                             "value": 0,
                         },
                         {
-                            "time": 1,
+                            "time": time(second=1),
                             "value": 1,
                         },
                         {
-                            "time": 2,
+                            "time": time(second=2),
                             "value": 2,
                         },
                     ],
@@ -118,41 +122,44 @@ class ResampleTransform(unittest.TestCase):
         )
         transformed_data = transform(data_frame.lazy()).collect()
 
+        expected_data = pl.DataFrame(
+            {
+                "feature_a": [
+                    [
+                        {
+                            "time": time(second=0),
+                            "value": 1.0,
+                        },
+                        {
+                            "time": time(second=1),
+                            "value": 1.5,
+                        },
+                        {
+                            "time": time(second=2),
+                            "value": 2.0,
+                        },
+                    ],
+                ],
+                "feature_b": [
+                    [
+                        {
+                            "time": time(second=0),
+                            "value": 0.0,
+                        },
+                        {
+                            "time": time(second=2),
+                            "value": 2.0,
+                        },
+                    ],
+                ],
+                "scalar": [42],
+            },
+        )
+
         assert_frame_equal(
             transformed_data,
-            pl.DataFrame(
-                {
-                    "feature_a": [
-                        [
-                            {
-                                "time": 0.0,
-                                "value": 1.0,
-                            },
-                            {
-                                "time": 1.0,
-                                "value": 1.5,
-                            },
-                            {
-                                "time": 2.0,
-                                "value": 2.0,
-                            },
-                        ],
-                    ],
-                    "feature_b": [
-                        [
-                            {
-                                "time": 0.0,
-                                "value": 0.0,
-                            },
-                            {
-                                "time": 2.0,
-                                "value": 2.0,
-                            },
-                        ],
-                    ],
-                    "scalar": [42],
-                },
-            ),
+            expected_data,
+            check_column_order=False,
         )
 
 
