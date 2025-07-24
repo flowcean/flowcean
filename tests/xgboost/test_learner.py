5d0fff69247f7fb345095c26a602f36b7fc0cd60
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2025-07-24 09:06:14 +0200

    --wip--

---
+++ tests/xgboost/test_learner.py
@@ -0,0 +1,32 @@
+import unittest
+
+import polars as pl
+from polars.testing import assert_frame_equal
+
+from flowcean.xgboost.learner import XGBoostLearner
+
+
+class XGBoostLearnerTest(unittest.TestCase):
+    def test_learn_and_predict(self):
+        df = pl.DataFrame(
+            {
+                "x": [1, 2, 3, 4],
+                "y": [0, 1, 0, 1],
+            },
+        )
+
+        model = XGBoostLearner().learn(
+            df.lazy().select("x"),
+            df.lazy().select("y"),
+        )
+
+        predictions = model.predict(df.lazy().select("x")).collect()
+
+        assert_frame_equal(
+            predictions,
+            df.select("y"),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
