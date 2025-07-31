3a699cbec694483a02b579e0caf79a402078b04b
Jan Christian Wieck <jan.christian.wieck@cml.fraunhofer.de>
2025-07-31 10:30:48 +0200

    Add tests for regressor

--- tests/xgboost/test_model.py
+++ tests/xgboost/test_model.py
@@ -5,7 +5,10 @@ import polars as pl
 from polars.testing import assert_frame_equal
 
 from flowcean.core.model import Model
-from flowcean.xgboost.learner import XGBoostClassifierLearner
+from flowcean.xgboost.learner import (
+    XGBoostClassifierLearner,
+    XGBoostRegressorLearner,
+)
 
 
 class XGBoostClassifierModelTest(unittest.TestCase):
@@ -19,6 +22,45 @@ class XGBoostClassifierModelTest(unittest.TestCase):
 
         # We still need to learn a model...
         model = XGBoostClassifierLearner(
+            objective="reg:squarederror",
+        ).learn(
+            df.lazy().select("x"),
+            df.lazy().select("y"),
+        )
+
+        # Save the model to a file / binary blob
+        blob = BytesIO()
+        model.save(blob)
+
+        # Now load the model and
+        blob.seek(0)  # Reset the stream position
+        loaded_model = Model.load(blob)
+
+        # Make a prediction with the original and loaded model and compare
+        # results
+
+        original_predictions = model.predict(df.lazy().select("x")).collect()
+        loaded_predictions = loaded_model.predict(
+            df.lazy().select("x"),
+        ).collect()
+
+        assert_frame_equal(
+            original_predictions,
+            loaded_predictions,
+        )
+
+
+class XGBoostRegressorModelTest(unittest.TestCase):
+    def test_learn_and_predict(self) -> None:
+        df = pl.DataFrame(
+            {
+                "x": [1.0, 2.0, 3.0, 4.0],
+                "y": [1.0, 4.0, 9.0, 16.0],
+            },
+        )
+
+        # We still need to learn a model...
+        model = XGBoostRegressorLearner(
             objective="reg:squarederror",
         ).learn(
             df.lazy().select("x"),
