b0f01ca0c47ddc4dd36e503573c391fb9b79934c
Maximilian Schmidt <maximilian@schmidt.so>
2025-09-08 14:51:32 +0200

    Make `output_names` a keyword argument (#346)

--- src/flowcean/sklearn/model.py
+++ src/flowcean/sklearn/model.py
@@ -1,34 +1,64 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import TYPE_CHECKING, Any, Protocol
 
 import polars as pl
 from typing_extensions import override
 
-from flowcean.core import Model
+from flowcean.core.model import Model
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+    from numpy.typing import NDArray
+
+
+class SupportsPredict(Protocol):
+    """Protocol describing an object that has a `predict` method."""
+
+    def predict(self, X: Any) -> NDArray: ...  # noqa: N803 X is a standard name in sklearn
 
 
 class SciKitModel(Model):
     """A model that wraps a scikit-learn model."""
 
+    estimator: SupportsPredict
+    output_names: list[str]
+
     def __init__(
         self,
-        model: Any,
-        output_name: str,
+        estimator: SupportsPredict,
+        *,
+        output_names: Iterable[str],
+        name: str | None = None,
     ) -> None:
         """Initialize the model.
 
         Args:
-            model: The scikit-learn model.
-            output_name: The name of the output column.
+            estimator: The scikit-learn estimator.
+            output_names: The names of the output columns.
+            name: The name of the model.
         """
-        self.model = model
-        self.output_name = output_name
+        if name is None:
+            name = estimator.__class__.__name__
+        self._name = name
+        self.estimator = estimator
+        self.output_names = list(output_names)
 
     @override
     def predict(
         self,
-        input_features: pl.LazyFrame,
+        input_features: pl.DataFrame | pl.LazyFrame,
     ) -> pl.LazyFrame:
-        outputs = self.model.predict(input_features.collect())
-        return pl.DataFrame({self.output_name: outputs}).lazy()
+        if isinstance(input_features, pl.LazyFrame):
+            input_features = input_features.collect()
+
+        outputs = self.estimator.predict(input_features)
+        if len(self.output_names) == 1:
+            data = {self.output_names[0]: outputs}
+        else:
+            data = {
+                self.output_names[i]: outputs[:, i]
+                for i in range(len(self.output_names))
+            }
+        return pl.LazyFrame(data)
