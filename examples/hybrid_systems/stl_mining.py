bb153d17c3d5a08f4c19423ac41913c11c58bf18
Ulrike Engeln <ulrike.engeln@tuhh.de>
2025-11-17 16:56:46 +0100

    final guards

--- examples/hybrid_systems/stl_mining.py
+++ examples/hybrid_systems/stl_mining.py
@@ -1,83 +1,36 @@
 from dynamic_hybrid_system import Bound
 
-STL_BOILER_TRANSITIONS = {
-    ("cooling", "heating"): [
-        {
-            "x0": Bound(left=None, right=20.002998748217117),
-            "t": Bound(left=1.5098182821386332, right=None),
-        },
-    ],
-    ("heating", "cooling"): [
-        {"t": Bound(left=1.008977239421488, right=None)},
-        {"x0": Bound(left=20.00015945738306, right=None)},
-    ],
-}
+STL_BOILER_TRANSITIONS = {('cooling', 'heating'): [{'t': Bound(left = 1.509, right = None),
+                           'x0': Bound(left = None, right = 20.026)}],
+ ('heating', 'cooling'): [{'t': Bound(left = 1.01, right = None)},
+                          {'x0': Bound(left = 20.0, right = None)}]}
 
-STL_BOILERNOTIME_TRANSITIONS = {
-    ("heating", "cooling"): [
-        {"x0": Bound(left=22.001578094079846, right=None)},
-    ],
-    ("cooling", "heating"): [
-        {"x0": Bound(left=None, right=18.00151967428954)},
-    ],
-}
+STL_BOILERNOTIME_TRANSITIONS = {('heating', 'cooling'): [{'x0': Bound(left = 22.002, right = None)}],
+ ('cooling', 'heating'): [{'x0': Bound(left = None, right = 18.002)}]}
 
 STL_TANK_TRANSITIONS = {
-    ("flow_2", "all_leak"): [
-        {
-            "x2": Bound(left=None, right=14.4512353337818),
-            "t": Bound(left=1.0099984961565491, right=None),
-        },
-    ],
-    ("flow_2", "flow_1"): [
-        {
-            "x2": Bound(left=12.99367224062909, right=None),
-            "t": Bound(left=1.0051409446468613, right=None),
-            "x1": Bound(left=None, right=2.3973562848676453),
-        },
-    ],
-    ("all_leak", "flow_1"): [
-        {"x1": Bound(left=None, right=5.102071903064735)},
-    ],
-    ("flow_1", "flow_2"): [
-        {
-            "x1": Bound(left=12.666840121840305, right=None),
-            "x2": Bound(left=None, right=2.01028058475264),
-            "t": Bound(left=1.0058590395229354, right=None),
-        },
-    ],
-    ("all_leak", "flow_2"): [
-        {"x2": Bound(left=None, right=5.066374375075659)},
-    ],
-    ("flow_2", "flow_0"): [
-        {
-            "x2": Bound(left=11.81077443019694, right=None),
-            "t": Bound(left=1.0004827758384582, right=None),
-            "x0": Bound(left=None, right=4.960610593460949),
-        },
-    ],
-    ("flow_1", "flow_0"): [
-        {
-            "x0": Bound(left=1.7675438438934794, right=None),
-            "t": Bound(left=1.0099607370292447, right=None),
-            "x1": Bound(left=None, right=14.701386103378294),
-        },
-    ],
-    ("all_leak", "flow_0"): [
-        {"x0": Bound(left=None, right=5.042491415035765)},
-    ],
-    ("flow_0", "flow_2"): [
-        {
-            "x0": Bound(left=12.279978715429017, right=None),
-            "x2": Bound(left=None, right=4.777410193758175),
-            "t": Bound(left=1.0033787886337568, right=None),
-        },
-    ],
-    ("flow_0", "flow_1"): [
-        {
-            "x0": Bound(left=11.56109109257665, right=None),
-            "t": Bound(left=1.002330274867684, right=None),
-            "x1": Bound(left=None, right=4.970656345527218),
-        },
-    ],
-}
+ ('flow_1', 'flow_0'): [{'x0': Bound(left = None, right = 4.78),
+                         't': Bound(left = 0.5, right = None),
+                         'x1': Bound(left = 9.778, right = None)}],
+('flow_0', 'flow_2'): [{'x2': Bound(left = None, right = 4.027),
+                         'x0': Bound(left = 9.774, right = None),
+                         't': Bound(left = 0.501, right = None)}],
+ ('flow_1', 'flow_2'): [{'x2': Bound(left = None, right = 4.017),
+                         't': Bound(left = 0.501, right = None),
+                         'x1': Bound(left = 9.786, right = None)}],
+ ('flow_2', 'flow_0'): [{'x0': Bound(left = None, right = 4.973),
+                         'x2': Bound(left = 9.746, right = None),
+                         't': Bound(left = 0.5, right = None)}],
+ ('flow_0', 'all_leak'): [{'x0': Bound(left = 9.842, right = None),
+                           't': Bound(left = 0.501, right = None)}],
+ ('all_leak', 'flow_1'): [{'x1': Bound(left = None, right = 5.074)}],
+ ('flow_2', 'all_leak'): [{'x2': Bound(left = None, right = 11.108),
+                           't': Bound(left = 0.5, right = None)}],
+ ('all_leak', 'flow_2'): [{'x2': Bound(left = None, right = 5.031)}],
+ ('all_leak', 'flow_0'): [{'x0': Bound(left = None, right = 5.042)}],
+ ('flow_2', 'flow_1'): [{'x2': Bound(left = 9.939, right = None),
+                         't': Bound(left = 0.401, right = None),
+                         'x1': Bound(left = None, right = 4.557)}],
+ ('flow_0', 'flow_1'): [{'x0': Bound(left = 9.869, right = None),
+                         't': Bound(left = 0.5, right = None),
+                         'x1': Bound(left = None, right = 4.504)}]}
