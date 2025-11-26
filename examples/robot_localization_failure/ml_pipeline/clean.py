c7425509fc73933eb53695c9e9fb850092791b40
Markus Knitt <markus.knitt@tuhh.de>
2025-11-26 16:58:35 +0100

    Train xgboost model on robotics example

--- examples/robot_localization_failure/ml_pipeline/clean.py
+++
@@ -1,4 +0,0 @@
-from ml_pipeline.utils.cleaning import clean_artifacts
-
-if __name__ == "__main__":
-    clean_artifacts()
--- examples/robot_localization_failure/ml_pipeline/utils/cleaning.py
+++
@@ -1,9 +0,0 @@
-import shutil
-from .paths import ARTIFACTS
-
-def clean_artifacts():
-    if ARTIFACTS.exists():
-        shutil.rmtree(ARTIFACTS)
-        print("✔ Deleted artifacts/")
-    ARTIFACTS.mkdir(parents=True, exist_ok=True)
-    print("✔ Recreated artifacts/")
