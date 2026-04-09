61c717716b335699662480180e37684be2deaeb4
Ulrike Engeln <ulrike.engeln@tuhh.de>
2026-04-09 18:00:49 +0200

    add anti windup pi control

--- examples/hybrid_systems/benchmarks/turbine.py
+++ examples/hybrid_systems/benchmarks/turbine.py
@@ -6,15 +6,15 @@ import numpy as np
 
 from flowcean.ode import Guard, HybridSystem, Mode, Transition
 
+from benchmarks.pid_controlled_plant import _plant_flow
+
 J_H     = 115926   # kgm^2                     Hub Inertia About Shaft Axis
 J_B     = 11776047 # kgm^2                     Second Mass Moment of Inertia (w.r.t. Root)
-J_G     = 534.116   # kgm^2                     Generator Inertia About High-Speed Shaft
+J_G     = 534.116  # kgm^2                     Generator Inertia About High-Speed Shaft
 xt0 = 0.7452
 omega0 = 1.2671
 theta0 = 0.143
-mas=[]
-fas=[]
-ts_ratios = []
+pitch_error_integral0 = 0.32997
 
 class polymodel:
     def __init__(self, modelterms, coefficients, parametervar, parameterstd, r2, adjustedr2, rmse):
@@ -25,6 +25,23 @@ class polymodel:
         self.r2 = r2
         self.adjustedr2 = adjustedr2
         self.rmse = rmse
+
+    def polyvaln(self, indepvars):
+        n, p = indepvars.shape
+        if n == 1 and self.modelterms.shape[1] == 1:
+            indepvars = indepvars.T
+            n, p = indepvars.shape
+        if self.modelterms.shape[1] != p:
+            raise ValueError("Number of independent variables does not match model terms.")
+        nt = self.modelterms.shape[0] 
+        ypred = np.zeros((n,1))
+        for i in range (nt):
+            t = np.ones((n,1))
+            for j in range(p):
+                t = t * indepvars[:,j]**self.modelterms[i,j]
+            ypred = ypred + self.coefficients[i] * t
+        return ypred
+
 ### from Schuler et al.
 cP_modelrm = polymodel(
     modelterms = np.array([[3,2,2,1,1,1,0,0,0,0],[0,1,0,2,1,0,3,2,1,0]]).T,
@@ -54,8 +71,14 @@ def turbine(
     gearbox_ratio: float = 1/97,
     cTe: float = 19325.0,
     mTe: float = 480650.0,
-    kTe: float = 1942400.0 ,
-    xT0: float = xt0 ,
+    kTe: float = 1942400.0,
+    omega_g_rated = 122.91,
+    pitch_kp: float = 0.018827,
+    pitch_ti: float = 2.33334,
+    pitch_antiwindup:float = 1,
+    pitch_min:float = 0.0,
+    pitch_max:float = 1.5708,
+    xT0: float = xt0,
     # paper != matlab implementation (*vs/) (gearbox_ratio**2),
     inertia: float = J_H + 3*J_B + J_G *((1/97)**2), # gearbox_ratio: float = 1/97,
     initial_state: np.ndarray | None = None,
@@ -74,23 +97,8 @@ def turbine(
         HybridSystem with time-dependent guard surfaces.
     """
     
-    def polyvaln(model, indepvars):
-        n, p = indepvars.shape # shape(1,2)
-        if n == 1 and model.modelterms.shape[1] == 1: # shape(10,2)
-            indepvars = indepvars.T
-            n, p = indepvars.shape
-        if model.modelterms.shape[1] != p:
-            raise ValueError("Number of independent variables does not match model terms.")
-
-        nt = model.modelterms.shape[0] 
-        ypred = np.zeros((n,1))
-        for i in range (nt):
-            t = np.ones((n,1))
-            for j in range(p):
-                t = t * indepvars[:,j]**model.modelterms[i,j]
-            ypred = ypred + model.coefficients[i] * t
-        return ypred
-
+    """ helper functions """
+    
     def tipspeed_ratio(omega, rotor_radius, wind_speed, dx):
         ts_ratio = omega * rotor_radius / (wind_speed - dx)
         return ts_ratio
@@ -108,23 +116,44 @@ def turbine(
         return fa
     
     def cp(tipspeed_ratio_val, theta):
-        a = polyvaln(cP_modelrm, np.array([[tipspeed_ratio_val, theta]])) #shape (1,2)
+        a = cP_modelrm.polyvaln(np.array([[tipspeed_ratio_val, theta]]))
         return np.squeeze(a)
     
     def c_thrust(tipspeed_ratio_val, theta):
-        a = polyvaln(cT_modelrm, np.array([[tipspeed_ratio_val, theta]])) #shape (1,2)
+        a = cT_modelrm.polyvaln(np.array([[tipspeed_ratio_val, theta]]))
         return np.squeeze(a)
+    
+    """ end helper functions """
 
-    def flow_(
+    def control_pitch(error:float, pitch_error_integral: float,  kp:float
+    ):
+        target = pitch_error_integral + kp*error
+        return target
+    
+    def _flow_plant(
         time: float,
-        state: np.ndarray, # omega, x, dx, theta, dtheta, target_torque, target_pitch
+        state: np.ndarray, # omega, x, dx, theta, dtheta, integral_pitch_error
         params: Mapping[str, float],
     ) -> np.ndarray:
-        omega, x, dx, theta, dtheta = state
-        # omega, x, dx, theta, dtheta, target_torque, target_pitch = state
+        """ general flow of the plant
+        clamp: if None, the controller output is used, else the clamp value is used as the controller output (for saturation modes)
+        """
+        omega, x, dx, theta, dtheta, pitch_error_integral = state
         wind_speed = 10
         target_torque = 1000
-        target_pitch = 0.1
+
+
+        error = omega/params["gearbox_ratio"] - params["omega_g_rated"]
+        u_ctrl = control_pitch(error, pitch_error_integral, params["pitch_kp"])
+        if u_ctrl > params["pitch_max"]:
+            target_pitch = params["pitch_max"]
+        elif u_ctrl < params["pitch_min"]:
+            target_pitch = params["pitch_min"]
+        else:
+            target_pitch = u_ctrl
+
+        target_pitch_diff = u_ctrl - target_pitch
+
         return np.array(
             [
                 # cp normally function of lambda, theta
@@ -132,26 +161,22 @@ def turbine(
                 dx, # x
                 (Fa(params["rho"], params["rotor_radius"], theta, wind_speed, omega, dx) - params["cTe"] * dx - params["kTe"]*(x - params["xT0"])) / params["mTe"], # dx
                 dtheta, # theta
-                -2*params["damping_pitch"] * dtheta - params["freq_pitch"]**2 * (theta - target_pitch), # dtheta (3) in Schuler et al.
-                # 0, # target_torque
-                # 0, # target_pitch
+                -2*params["damping_pitch"] * dtheta - params["freq_pitch"]**2 * (theta - target_pitch), # dtheta: 2nd order lag (3) in Schuler et al.
+                (params["pitch_kp"]*error-target_pitch_diff)/params["pitch_ti"], # pitch_error_integral
             ],
             dtype=float,
         )
 
-   
-    left = Mode(name="left", flow=flow_)
-
-   
-
+    operation = Mode(name="operation", flow=_flow_plant)
+    transitions = []
     if initial_state is None:
-        initial_state = np.array([omega0, xt0, 0, theta0, 0], dtype=float)
-        # initial_state = np.array([120, 0, 0, 0.1, 0, 1000, 0.1], dtype=float)
+        initial_state = np.array([omega0, xt0, 0, theta0, 0, pitch_error_integral0], dtype=float)
 
     return HybridSystem(
-        modes={"left": left},
-        transitions=[],
-        initial_mode="left",
+        # modes={"unclamped": unclamped, "sat_low": sat_low, "sat_high": sat_high},
+        modes={"operation": operation},
+        transitions=transitions,
+        initial_mode="operation",
         initial_state=initial_state,
         params={
             "damping_pitch": damping_pitch,
@@ -164,5 +189,11 @@ def turbine(
             "kTe": kTe,
             "xT0": xT0,
             "inertia": inertia,
+            "omega_g_rated": omega_g_rated,
+            "pitch_kp": pitch_kp,
+            "pitch_ti": pitch_ti,
+            "pitch_antiwindup": pitch_antiwindup,
+            "pitch_min": pitch_min,
+            "pitch_max": pitch_max,
         },
     )
