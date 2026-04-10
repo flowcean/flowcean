c158fc53f0e2668ae33a6043d34664a10bee181e
Ulrike Engeln <ulrike.engeln@tuhh.de>
2026-04-10 10:58:11 +0200

    remove unnecessary polymodel params

--- examples/hybrid_systems/benchmarks/turbine.py
+++ examples/hybrid_systems/benchmarks/turbine.py
@@ -17,14 +17,9 @@ theta0 = 0.143
 pitch_error_integral0 = 0.32997
 
 class polymodel:
-    def __init__(self, modelterms, coefficients, parametervar, parameterstd, r2, adjustedr2, rmse):
+    def __init__(self, modelterms, coefficients):
         self.modelterms = modelterms
         self.coefficients = coefficients
-        self.parametervar = parametervar
-        self.parameterstd = parameterstd
-        self.r2 = r2
-        self.adjustedr2 = adjustedr2
-        self.rmse = rmse
 
     def polyvaln(self, indepvars):
         n, p = indepvars.shape
@@ -46,20 +41,10 @@ class polymodel:
 cP_modelrm = polymodel(
     modelterms = np.array([[3,2,2,1,1,1,0,0,0,0],[0,1,0,2,1,0,3,2,1,0]]).T,
     coefficients = [-4.645506812711324e-04,-0.074780487295004,-0.002458175859067,-0.372740535667247,0.071114752590880,0.175932463831508,3.898087853290869,-3.514363457408013,1.599390296654579,-0.404129012120541],
-    parametervar = [7.501388136040788e-10,6.679452782066278e-08,1.539190796429710e-07,7.258526274162107e-06,7.617687742264648e-06,3.419686114249399e-06,5.452288307592214e-04,4.709605455918789e-04,7.816513374422656e-05,8.319146166316219e-06],
-    parameterstd = [2.738866213607519e-05,2.584463732008302e-04,3.923252217777631e-04,0.002694165227703,0.002760015895292,0.001849239333956,0.023350135561902,0.021701625413592,0.008841104780751,0.002884293009789],
-    r2 = 0.9885,
-    adjustedr2 = 0.9885,
-    rmse = 0.97
 )
 cT_modelrm = polymodel(
     modelterms = np.array([[3,2,2,1,1,1,0,0,0,0],[0,1,0,2,1,0,3,2,1,0]]).T,
     coefficients = [0.001682233589763,-0.005151180495985,-0.047219724810382,1.164758256848169,-1.525761648555954,0.553206918519747,6.914278352507001,-10.967542962838312,6.000636278248273,-0.980172788618940],
-    parametervar = [7.673725840212874e-09,6.832907254320090e-07,1.574552332631929e-06,7.425284443593678e-05,7.792697326196755e-05,3.498250353724942e-05,0.005577549770187,0.004817804442174,7.996090799980241e-04,8.510271132119102e-05],
-    parameterstd = [8.759980502382910e-05,8.266140123612768e-04,0.001254811672177,0.008617009019140,0.008827625573277,0.005914600877257,0.074682995187575,0.069410405863778,0.028277359848437,0.009225113079046],
-    r2 = 0.9924,
-    adjustedr2 = 0.9924,
-    rmse = 0.0311
 )
 ###
 
@@ -148,14 +133,14 @@ def turbine(
         # calculate aerodynamic force
         # Schuler et al. Eq (2)
         tipspeedratio = tipspeed_ratio(omega, rotor_radius, wind_speed, dx)
-        fa = 1/2 * rho * np.pi * rotor_radius**2 * c_thrust(tipspeedratio, theta) * (wind_speed - dx)**2
+        fa = 1/2 * rho * np.pi * rotor_radius**2 * ct(tipspeedratio, theta) * (wind_speed - dx)**2
         return fa
     
     def cp(tipspeed_ratio_val, theta):
         a = cP_modelrm.polyvaln(np.array([[tipspeed_ratio_val, theta]]))
         return np.squeeze(a)
     
-    def c_thrust(tipspeed_ratio_val, theta):
+    def ct(tipspeed_ratio_val, theta):
         a = cT_modelrm.polyvaln(np.array([[tipspeed_ratio_val, theta]]))
         return np.squeeze(a)
     
@@ -211,7 +196,6 @@ def turbine(
             # zone 2.5: transition 
             target_torque = params["torque_vs_slope25"]*(genspeed_f - params["torque_vs_sysp"])
         ###
-        
         return np.array(
             [
                 ((Ma(params["rho"], params["rotor_radius"], theta, wind_speed, omega, dx) - target_torque/(params["gearbox_ratio"])) / params["inertia"]), # omega from Schuler et al. Eq (1a)
